08d8847a6146dba2cf2232c7d8444d0427f6f9ab
Miguel Marinho <miguel.marinho.silva@outlook.com>
2024-02-22 20:41:58 +0000

    data_updates

--- lib/data.ts
+++ lib/data.ts
@@ -44,7 +44,7 @@ export const experiencesData = [
     date: "2017",
   },
   {
-    title: "Bsc in Economics",
+    title: "Bachelors in Economics",
     location: "Nova Lisbon SBE",
     description:
       "I graduated with an Economics bachelors",
@@ -55,44 +55,44 @@ export const experiencesData = [
     title: "Master of Analytics Specialization in Data Science",
     location: "Católica Lisbon SBE",
     description:
-      "I graduated with a data science applied to business masters, where I truly fell passionate to the arts of Data Science, more specifically Machine Learning",
+      "I graduated with a data science applied to business masters, where I truly fell passionate to the arts of Data Science",
     icon: React.createElement(LuGraduationCap),
     date: "2021-2023",
   },
   {
     title: "Data Science Trainee in Rotational Program",
-    location: "NOKIA",
+    location: "NOKIA - Supply Chain Advanced Analytics Team  ",
     description:
-      "Rotational program-- falar das 4 fasess",
-    icon: React.createElement(CgWorkAlt),
-    date: "2022 - 2023",
+"   Involved in classification project using Azure Cloud Machine Learning Studio to accurately predict on-time delivery of supply chain products. Migrated data pipelines from on-premises databases to Azure SQL Database, leveraging Azure Data Factory."
+    ,icon: React.createElement(CgWorkAlt),
+    date: "2022 Sep - 2022 Dec",
   },
   
   {
-    title: "Data Science Trainee",
-    location: "NOKIA",
+    title: "Data Science Trainee in Rotational Program",
+    location: "NOKIA - Process Mining Team ",
     description:
-      "Rotational program-- falar das 4 fasess",
+      "Automating daily data extraction and API updates from Celonis API using Azure Function Apps. Automated extraction, processing, and ingestion of Celonis audit logs API data into data pools using Python. Developed star schema, associated tables (SQL), & dashboard.  Classification project using Celonis Machine Learning Workbench: Predict discount classification: Descriptive analysis using Matplotlib, coded using skicit-learn  ",
     icon: React.createElement(CgWorkAlt),
-    date: "2022 - 2023",
+    date: "2022 Dec - 2023 Feb",
   },
   
   {
-    title: "Data Science Trainee",
-    location: "NOKIA",
+    title: "Data Science Trainee in Rotational Program",
+    location: "NOKIA - Data Semantics Team",
     description:
-      "Rotational program-- falar das 4 fasess",
+      "Contributed on data migration tasks from on-premises server to Azure cloud by doing User Acceptance Testing (UAT) with SQL. Lead in developing a Python-based Excel File Comparator program, ensuring data consistency between on-premise servers and Azure cloud by precisely locating and reporting any discrepancies. ",
     icon: React.createElement(CgWorkAlt),
-    date: "2022 - 2023",
+    date: "2022 Mar - 2023 May",
   },
   
   {
-    title: "Data Science Trainee",
-    location: "NOKIA",
+    title: "Data Science Trainee in Rotational Program",
+    location: "NOKIA - Financial Planning and Reporting Analytics",
     description:
-      "Rotational program-- falar das 4 fasess",
+      "Lead in optimizing SAP report generation by recreating complex reports using advanced SQL queries on a centralized data platform with ingested SAP tables, significantly reducing processing time. Python scripts for automated validation of reports. ",
     icon: React.createElement(CgWorkAlt),
-    date: "2022 - 2023",
+    date: "2022 Jun - 2023 Sep",
   },
   {
     title: "Data Scientist/Engineer",
@@ -100,7 +100,7 @@ export const experiencesData = [
     description:"In charge of creating & enhancing complex supply chain data objects that can be used by business to facilitate end-to-end reporting, transforming their business requests to data solutions",
     
     icon: React.createElement(FaReact),
-    date: "2023 - present",
+    date: "2023 Sep - Present",
   },
 ] as const;
 
